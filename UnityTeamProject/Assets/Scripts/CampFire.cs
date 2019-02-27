7f79947d768d378d0f82ea3409db5f37347473b0
DIEGOTEC18 <a01027322@itesm.mx>
2019-02-27 08:20:03 -0600

    firstCommit

---
+++ UnityTeamProject/Assets/Scripts/CampFire.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CampFire : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int noSticks;
+    public float radius;
+    public GameObject myFire;
+    public bool fireStarted = false;
+    public Text text;
+    private int sticksRemaining;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+        int i = 0;
+        int count = 0;
+        for(i = 0; i < hitColliders.Length; i++)
+        {
+            if(hitColliders[i].gameObject.tag == "Stick")
+            {
+                count += 1;
+            }
+        }
+        sticksRemaining = noSticks - count;
+        if (count >= noSticks && !fireStarted)
+        {
+            fireStarted = true;
+        }
+        else {
+            if(fireStarted)
+            {
+                text.text = "Press X to start Fire!";
+            }
+            else
+            {
+                text.text = sticksRemaining + " sticks to go!";
+            }
+        }
+        count = 0;
+        if(Input.GetButton("Fire3") && fireStarted)
+        {
+            StartFire();
+        }
+    }
+
+    void StartFire()
+    {
+        Instantiate(myFire, transform.position, Quaternion.identity);
+    }
+}
---
+++ UnityTeamProject/Assets/Scripts/XboxController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XboxController : MonoBehaviour
+{
+    public static bool inRange;
+    public Transform stickHold;
+    public Transform myStick;
+    public bool isHolding = false;
+    public GameObject myHit;
+    public float forceStrength;
+    public Rigidbody rb;
+    private Collider col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Input.GetButtonUp("Fire1"))
+        {
+            if(isHolding)
+            {
+                
+                myStick.transform.parent = null;
+                col.enabled = true;
+                rb.isKinematic = false;
+                rb.AddForce(transform.forward * forceStrength);
+                col = null;
+                myStick = null;
+                rb = null;
+                isHolding = false;
+            }
+            if (Physics.Raycast(transform.position, transform.TransformDirection((Vector3.forward)), out hit))
+            {
+                print("Raycast hit");
+                
+                if (hit.collider.gameObject.tag == "Stick" && !isHolding)
+                {
+                    myStick = hit.collider.transform;
+                    rb = myStick.gameObject.GetComponent<Rigidbody>();
+                    col = myStick.gameObject.GetComponent<Collider>();
+                    myStick.position = stickHold.position;
+                    myStick.parent = stickHold;
+                    col.enabled = false;
+                    rb.isKinematic = true;
+                    isHolding = true;
+                }
+            }
+        }
+    }
+}
